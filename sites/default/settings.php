022b7b3f5f460f9f3adfefe85c45feffa745c811
benbeezy <benengel94@gmail.com>
2015-08-27 03:32:38 -0600

    sql

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -212,7 +212,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'sum',
+      'username' => 'root',
+      'password' => 'jello123',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -244,7 +258,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'W1unlhoQkbIq73EQtnn7OjYbJuXgdCVXdqXk4D8-qOc';
 
 /**
  * Base URL (optional).
